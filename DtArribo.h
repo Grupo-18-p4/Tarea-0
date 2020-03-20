abf67981fd371d89e41f92f94541fb2742eed64f
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-19 21:47:52 -0300

    Add files via upload

---
+++ DtArribo.h
@@ -0,0 +1,21 @@
+#ifndef DTARRIBO_H
+#define DTARRIBO_H
+
+#include "DtFecha.h"
+#include "DtBarco.h"
+#include <iostream>
+
+class DtArribo {
+	private:
+		DtFecha fecha;
+		float carga;
+		DtBarco barco;
+    public: 
+    	DtArribo();
+    	DtArribo(DtFecha fecha, float carga, DtBarco barco);
+    	DtFecha getF();
+    	float getC();
+    	DtBarco getB();
+};
+
+#endif
