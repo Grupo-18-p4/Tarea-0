58f990e3fbed1f684f1f73bf570005894e3b896c
vicente-fonfria <62406708+vicente-fonfria@users.noreply.github.com>
2020-03-24 17:47:14 -0300

    Create BarcoPesquero.cpp

---
+++ src/BarcoPesquero.cpp
@@ -0,0 +1,26 @@
+#include ".../include/Barco.h"
+#include <iostream>
+
+BarcoPesquero :: BarcoPesquero();
+
+BarcoPesquero :: BarcoPesquero(string nombre,string id, int capacidad, int carga): Barco(nombre,id){
+    this->capacidad = capacidad;
+    this->carga = carga;
+}
+
+BarcoPesquero :: BarcoPesqero(int capacidad){
+    this capacidad = capacidad;
+}
+
+BarcoPesquero :: BarcoPesquero(int carga){
+    this carga = carga;
+}
+
+capacidad BarcoPesquero :: getCapacidad(){
+    return this->capacidad;
+}
+
+carga BarcoPesquero :: getcarga(){
+    return this->carga;
+}
+
