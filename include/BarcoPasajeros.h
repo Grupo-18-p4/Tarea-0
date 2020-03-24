52cd64cfae38bb46c872d269928e2da0833f2a82
vicente-fonfria <62406708+vicente-fonfria@users.noreply.github.com>
2020-03-24 17:08:43 -0300

    Update BarcoPasajeros.h

--- include/BarcoPasajeros.h
+++ include/BarcoPasajeros.h
@@ -8,6 +8,12 @@ class BarcoPasajeros : public Barco
 
      public:
      
+     BarcoPasajeros();
+     BarcoPasajeros(String id,int cantPasajeros,TipoTamanio tamanio);
+     
+     int getCantPasajeros(String id);
+     TipoTamanio getTipoTamanio(String id);
+     
      void arribar(float cargaDespacho); //los barcos de pasajeros no poseen carga, no se realizan cambios
  };
  #endif
