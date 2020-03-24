5a4a904c4d0ecd68bdd3fbd3ce9fca667b5fb4ff
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-24 18:52:26 -0300

    Add files via upload

---
+++ principal.cpp
@@ -0,0 +1,14 @@
+#include "include/DtBarcoPesquero.h" //ESTO DESPUES HAY QUE SACARLO
+
+int main(){
+    int opcion = 21;
+    while(opcion != 0){
+        cout << "1)Agregar puerto" << '\n';
+        cout << "2)Agregar barco" << '\n';
+        cout << "3)Listar puertos" << '\n';
+        cout << "0) Salir" << '\n';
+        cout << ">> ";
+        cin >> opcion;
+    }
+  
+}
