4fa2bdd653e6a37352e7a5ff76da7d61f923a37a
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-24 17:59:02 -0300

    Add files via upload

--- include/DtBarco.h
+++ include/DtBarco.h
@@ -1,15 +1,16 @@
-#include <iostream> //Para poder usar el tipo string
 #ifndef _DtBarco_
 #define _DtBarco_
+#include <iostream> //Para poder usar el tipo string
+using namespace std;
 class DtBarco{
     private: 
-        std::string cantPasajeros;
-        std::string id;
+        string nombre;
+        string id;
     public:
-        DtBarco();
-        DtBarco(std::string cantPasajeros, std::string id);
-        std::string get_id();
-        std::string get_cantPasajeros();
+        //DtBarco(); Me parece que esto no es necesario
+        DtBarco(string nombre, string id);
+        string get_id();
+        string get_nombre();
         //NO SE SI VAN LOS SETTER
 };
 
--- include/DtBarcoPesquero.h
+++ include/DtBarcoPesquero.h
@@ -5,14 +5,16 @@ class DtBarcoPesquero: public DtBarco{
     private:
         int capacidad;
         int carga;
-    public:
-        
+    
+    public:     
         //Constructores
-        DtBarcoPesquero(); //No se si es necesario agregar este
-        DtBarcoPesquero(int capacidad, int carga);
+        DtBarcoPesquero(string nombre,string id, int capacidad, int carga);
         
         //Getters
         int get_capacidad();
         int get_carga();
+
+        //Destructor
+        ~DtBarcoPesquero();
 };
 #endif
