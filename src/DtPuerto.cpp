0003faf5b741e47dc36ef204507d813da37b1dcf
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-25 23:40:59 -0300

    Update DtPuerto.cpp

--- src/DtPuerto.cpp
+++ src/DtPuerto.cpp
@@ -4,29 +4,27 @@
 using namespace std;
 
 DtPuerto :: DtPuerto(){
-};
+}
 
 DtPuerto :: DtPuerto(string id, string nombre, DtFecha fechaCreacion, int cantArribos){
 	this->id = id;
 	this->nombre = nombre;
-	this->fechaCreacion->dia = fechaCreacion.getD();
-	this->fechaCreacion->mes = fechaCreacion.getM();
-	this->fechaCreacion->anio = fechaCreacion.getA();
+	this->fechaCreacion = fechaCreacion;
 	this->cantArribos = cantArribos;
-};
+}
 
-std::string DtPuerto :: getid(){
+string DtPuerto :: getid(){
 	return this->id;
-};
+}
 
-std::string DtPuerto :: getnombre(){
+string DtPuerto :: getnombre(){
 	return this->nombre;
-};
+}
 
 DtFecha DtPuerto :: getfecha(){
-	return this->fecha;
-};
+	return this->fechaCreacion;
+}
 
 int DtPuerto :: getcant(){
 	return this->cantArribos;
-};
+}
