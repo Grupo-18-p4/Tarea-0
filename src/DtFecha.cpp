1e75496102b2c414545119532fbc7e8e44ada09c
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-25 23:40:21 -0300

    Update DtFecha.cpp

--- src/DtFecha.cpp
+++ src/DtFecha.cpp
@@ -1,10 +1,11 @@
 #include "../include/DtFecha.h"
 #include "<iostream>"
+
 DtFecha :: DtFecha(){
-};
+}
 
 DtFecha :: DtFecha(int dia, int mes, int anio){
-	if (dia > 31) || (dia < 1) || (mes > 12) || (mes < 1) || (anio < 1900){
+	if ((dia > 31) || (dia < 1) || (mes > 12) || (mes < 1) || (anio < 1900)){
 		throw std::invalid_argument("No es válido."); 
 	}
 	else{
@@ -12,15 +13,15 @@ DtFecha :: DtFecha(int dia, int mes, int anio){
 	this->mes = mes;
 	this->anio = anio;
 	}
-};
+}
 
 int DtFecha :: getD(){
 	return this->dia;
-};
+}
 
 int DtFecha :: getM(){
 	return this->mes;
-};
+}
 
 int DtFecha :: getA(){
 	return this->anio;
