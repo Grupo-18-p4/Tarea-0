6a45d8e159137aa01875578b63a07f3af4b7625f
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-25 23:56:18 -0300

    Add files via upload

--- include/DtBarcoPasajeros.h
+++ include/DtBarcoPasajeros.h
@@ -1,19 +1,19 @@
 #ifndef _DtBarcoPasajeros_
 #define _DtBarcoPasajeros_
 #include "DtBarco.h"
-//#include "TipoTamanio.h"
+#include "TipoTamanio.h"
 class DtBarcoPasajeros: public DtBarco{
     private:
         int cantPasajeros;
-        //TipoTamanio tamanio;
+        TipoTamanio tamanio;
     public:
         
         //Constructores
-        DtBarcoPasajeros(); //No se si es necesario agregar este
-        //DtBarcoPasajeros(int cantPasajeros, TipoTamanio tamanio)
+        //DtBarcoPasajeros(); //No se si es necesario agregar este
+        DtBarcoPasajeros(string nombre, string id, int cantPasajeros, TipoTamanio tamanio);
         
         //Getters
         int get_cantPasajeros();
-        //TipoTamanio get_tamanio()
+        TipoTamanio get_tamanio();
 };
 #endif
