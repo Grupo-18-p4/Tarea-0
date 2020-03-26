cb5b480a07fac9f61d0ca0ff14658b5c4942638e
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-25 23:41:56 -0300

    Update DtArribos.cpp

--- src/DtArribos.cpp
+++ src/DtArribos.cpp
@@ -4,26 +4,24 @@
 #include <iostream>
 
 DtArribo :: DtArribo(){
-};
+}
 
 DtArribo :: DtArribo(DtFecha fecha, float carga, DtBarco barco){
-	this->fecha->dia = fecha.getD();
-	this->fecha->mes = fecha.getM();     // o this->fecha = fecha;
-	this->fecha->anio = fecha.getA();
+	this->fecha = fecha;
 	this->carga = carga;
 	this->barco = barco;
-};
+}
 
 DtFecha DtArribo :: getF(){
 	return this->fecha;
-};
+}
 
 float DtArribo :: getC(){
 	return this->carga;
-};
+}
 
 DtBarco getB(){
 	return this->barco;
-};
+}
 
 
