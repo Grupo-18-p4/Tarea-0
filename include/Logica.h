cecf9e9502680d81ed0eece1be706c6c880edcdf
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-26 23:50:45 -0300

    Add files via upload

---
+++ include/Logica.h
@@ -0,0 +1,20 @@
+#ifndef _Logica_
+#define _Logica_
+//FALTA ARRIBO
+#include "BarcoPesquero.h"
+#include "BarcoPasajeros.h"
+#include "DtArribo.h"
+#include "DtBarcoPesquero.h"
+#include "DtBarcoPasajeros.h"
+#include "DtFecha.h"
+#include "DtPuerto.h"
+//Falta PUERTO
+#include "TipoTamanio.h"
+
+struct array_barcos;
+
+
+void agregarBarco(DtBarco& barco);
+
+
+#endif
