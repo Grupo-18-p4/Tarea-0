dde84fff497d9756a9dd98c8fb6991c614bafef8
lucasfalce <62406370+lucasfalce@users.noreply.github.com>
2020-03-27 19:52:13 -0300

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -1,14 +1,50 @@
 #include "include/DtBarcoPesquero.h" //ESTO DESPUES HAY QUE SACARLO
+#include <stdlib.h>
 
 int main(){
     int opcion = 21;
     while(opcion != 0){
-        cout << "1)Agregar puerto" << '\n';
-        cout << "2)Agregar barco" << '\n';
-        cout << "3)Listar puertos" << '\n';
+        cout << "1) Agregar puerto" << '\n';
+        cout << "2) Agregar barco" << '\n';
+        cout << "3) Listar puertos" << '\n';
+        cout << "4) Agregar arribo" << '\n';
+        cout << "5) Obtener arribos en puerto" << '\n';
+        cout << "6) Eliminar arribos" << '\n';
+        cout << "7) Listar barcos" << '\n';
         cout << "0) Salir" << '\n';
         cout << ">> ";
         cin >> opcion;
-    }
-  
-}
+        switch (opcion){
+                
+    case 1: //Agregar puerto
+        break;
+
+    case 2: //Agregar barco
+        break;
+                
+    case 3: //Listar puertos
+     break;
+                
+    case 4: //Agregar arribo
+     break;
+                
+    case 5: //Obtener arribos en puerto
+     break;
+            
+    case 6: //Eliminar arribos
+     break;
+                
+    case 7: //Listar barcos
+     break;  
+                
+    case 0: //Salir
+      cout << "Saliendo...";
+                Exit;
+     break; 
+                
+    default:
+        cout << "Opción no válida, volviendo...";
+     break;
+        }//switch
+    } //While
+} // Main
