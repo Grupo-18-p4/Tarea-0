910c565131f398680dcc2f8df2c8f1e9a3523e29
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-28 11:04:01 -0300

    Update Arribo.h

--- include/Arribo.h
+++ include/Arribo.h
@@ -1,13 +1,20 @@
-class Arribo 
- {    
+#ifndef _Arribo_
+#define _Arribo_
+#include <iostream>
+#include "DtFecha.h"
+using namespace std;
+
+
+class Arribo {    
      private:  
      DtFecha id;    
      float carga;
  
  public:
- DtFecha GetArriboFecha(Arribo arrival);
-  string GetArriboCarga(Arribo arrival);
- Arribo::~Arribo();
- Arribo::Arribo();
- Arribo :: Arribo(DtFecha fecha,float carga);
- } 
+    DtFecha GetArriboFecha(Arribo arrival);
+    string GetArriboCarga(Arribo arrival);
+    Arribo::~Arribo();
+    Arribo::Arribo();
+    Arribo :: Arribo(DtFecha fecha,float carga);
+ };
+ #endif
