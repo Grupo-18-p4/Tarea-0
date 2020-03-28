9bdf6caa0e2def28d1f8ffc11db5442b33e6d16b
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-28 20:20:46 -0300

    Update Logica.h

--- include/Logica.h
+++ include/Logica.h
@@ -15,7 +15,7 @@
 struct array_barcos;
 struct Array_puertos;
 
-bool controlIdBarco(string id);
+bool idPuertoRepetido(string id);
 void  agregarBarco(DtBarco& barco);
 void controlidPuerto(string id);
 void agregarPuerto(string id, string nombre, const DtFecha& fechaCreacion);
