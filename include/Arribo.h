27dbafbbabc2a399dbb0b12f3330af6a76a4d610
lucasfalce <62406370+lucasfalce@users.noreply.github.com>
2020-03-28 10:57:05 -0300

    Update Arribo.h

--- include/Arribo.h
+++ include/Arribo.h
@@ -3,4 +3,11 @@ class Arribo
      private:  
      DtFecha id;    
      float carga;
+ 
+ public:
+ DtFecha GetArriboFecha(Arribo arrival);
+  string GetArriboCarga(Arribo arrival);
+ Arribo::~Arribo();
+ Arribo::Arribo();
+ Arribo :: Arribo(DtFecha fecha,float carga);
  } 
