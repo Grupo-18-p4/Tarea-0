950ff72fb81308498dacf4746cbe5d5428f145ef
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-28 12:17:15 -0300

    Add files via upload

--- include/Puerto.h
+++ include/Puerto.h
@@ -4,19 +4,18 @@
 #include <iostream>
 #include "DtFecha.h"
 using namespace std;
-class Puerto{  
-     
-     private:  
+class Puerto{   
+    private:  
         string id;  //field or data member     
         string nombre; //field or data member  
         DtFecha fechaCreacion;//field or data member
      
     public: 
-Puerto::Puerto();
-Puerto::Puerto(string id, string nombre, DtFecha fechaCreacion);
-Puerto::~Puerto();
-string Puerto::GetPuertoId(puerto port);
-string Puerto::GetPuertoNombre(puerto port);
-DtFecha Puerto::GetPuertoFecha(puerto port);
+        Puerto::Puerto();
+        Puerto::Puerto(string id, string nombre, DtFecha fechaCreacion);
+        Puerto::~Puerto();
+        string Puerto::GetPuertoId();
+        string Puerto::GetPuertoNombre();
+        DtFecha Puerto::GetPuertoFecha();
  };
  #endif
