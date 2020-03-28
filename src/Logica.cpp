b08579cdf0b11997cd59a5c7e96181d86e710f5f
lucasfalce <62406370+lucasfalce@users.noreply.github.com>
2020-03-28 12:36:19 -0300

    Update Logica.cpp

--- src/Logica.cpp
+++ src/Logica.cpp
@@ -1,15 +1,21 @@
 #include "../include/Logica.h"
-
+#include <string>
+#include <stdlib.h>
+#include <ctime>
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const int MAX_PUERTOS = 32;
 const int MAX_BARCOS = 32;
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct Array_barcos {
   DtBarco* arregloBarco[MAX_BARCOS];
   int tope = -1;
-};
+} aB;
 
-Array_barcos aB;
-
+struct Array_puertos {
+  Dtpuerto* arreglopuerto[MAX_PUERTOS];
+  int tope = -1;
+} aP;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void agregarBarco(DtBarco& barco){
     if(aB.tope < 0){
         aB.arregloBarco[0] = &barco;
@@ -19,7 +25,7 @@ void agregarBarco(DtBarco& barco){
         bool barcoRepetido = false;
         for(int i = 0; i++; i<=aB.tope){
           if(aB.arregloBarco[i]->get_id() == barco.get_id()){
-              throw std::invalid_argument("Ya existe barco con ese nombre."); 
+              throw std::invalid_argument("Ya existe barco con ese identificador."); 
               barcoRepetido = true;
               delete &barco;  
               break;
@@ -33,3 +39,34 @@ void agregarBarco(DtBarco& barco){
     }
 
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+controlidPuerto(String id){
+bool puertoRepetido = false;
+  if (aP.tope >-1){
+        for(int i = 0; i++; i<=aP.tope){
+          if(aP.arregloPuerto[i]->get_id() == id{
+              throw std::invalid_argument("Ya existe puerto con ese identificador."); 
+              puertoRepetido = true;}
+}
+    
+void agregarPuerto(string id, string nombre, const DtFecha& fechaCreacion){
+  //Se hace previamente el control del id
+  puerto port= new dtpuerto(id,nombre,DtFecha,0); 
+  
+  if(aP.tope < 0){
+        aP.arregloPuerto[0] = &port;
+        aP.tope++;
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+Col(DtPuerto) listarPuertos(){}
+void agregarArribo(string idPuerto, string idBarco, float cargaDespacho){}
+Col(DtArribo) obtenerInfoArribosEnPuerto(string idPuerto){}
+void eliminarArribos(string idPuerto, const DtFecha& fecha){}
+Col(DtBarco*)listarBarcos(){}
