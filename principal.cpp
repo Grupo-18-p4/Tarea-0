d1d1e9aa51184bfc42863dbe1a114848d4119e20
ibm5101 <62406528+ibm5101@users.noreply.github.com>
2020-03-28 18:22:41 -0300

    Add files via upload

--- principal.cpp
+++ principal.cpp
@@ -1,4 +1,4 @@
-#include "include/Logica.h" 
+#include "include/Logica.h"
 #include <iostream>
 
 /////////////////////////Variables////////////////////////////////
@@ -7,10 +7,12 @@ int dia, mes, anio;
 
 //////////////////////////////////////////////////////////////////
 
-int main(){
+int main()
+{
     int opcion = 21;
     int opc = 0;
-    while(opcion != 0){
+    while (opcion != 0)
+    {
         cout << "1) Agregar puerto" << '\n';
         cout << "2) Agregar barco" << '\n';
         cout << "3) Listar puertos" << '\n';
@@ -21,26 +23,29 @@ int main(){
         cout << "0) Salir" << '\n';
         cout << ">> ";
         cin >> opcion;
-        switch (opcion){
-                
-    case 1: //Agregar puerto
-    {
-        cout << "Ingrese el id del puerto";
-        cin >> id;               
-        cout << "Ingrese el nombre del puerto";
-        cin >> nombre;
-        controlidPuerto(nombre);
-        cout << "Ingrese fecha de creación del puerto. Formato: (DD MM AA)";
-        cin >> dia >> mes >> anio;
-        DtFecha FechaPuerto(dia,mes,anio);
-        agregarPuerto(id,nombre,FechaPuerto);
-        
-    }
-    break;
-    case 2: 
-                
-                cout << "Ingrese el id del barco: ";
-                cin >> id;
+        switch (opcion)
+        {
+
+        case 1: //Agregar puerto
+        {
+            cout << "Ingrese el id del puerto";
+            cin >> id;
+            cout << "Ingrese el nombre del puerto";
+            cin >> nombre;
+            controlidPuerto(nombre);
+            cout << "Ingrese fecha de creación del puerto. Formato: (DD MM AA)";
+            cin >> dia >> mes >> anio;
+            DtFecha FechaPuerto(dia, mes, anio);
+            agregarPuerto(id, nombre, FechaPuerto);
+        }
+        break;
+        case 2:
+            cout << "Ingrese el id del barco: ";
+            cin >> id;
+            try
+            {
+                if (controlIdBarco(id))
+                    throw std::invalid_argument("Ya existe barco con ese identificador.");
                 cout << "Ingrese el nombre del barco: ";
                 cin >> nombre;
                 cout << "De que tipo es el barco a ingresar?" << '\n';
@@ -48,65 +53,75 @@ int main(){
                 cout << "2)Pasajeros" << '\n';
                 cout << ">> ";
                 cin >> opc;
-                if(opc == 1){
+                if (opc == 1)
+                {
                     int capacidad, carga;
                     cout << "Capacidad: ";
                     cin >> capacidad;
                     cout << "Carga: ";
                     cin >> carga;
-                    DtBarco* barcoPes = new DtBarcoPesquero(nombre,id,capacidad,carga); 
-                    agregarBarco(*barcoPes);
+                    DtBarcoPesquero barcoPes(nombre, id, capacidad, carga);
+                    agregarBarco(barcoPes);
                 }
-                else if(opc == 2){
+                else if (opc == 2)
+                {
                     TipoTamanio tam;
                     int cantPasajeros;
                     cout << "Que tipo de tamaño: \n1)Bote\n2)Crucero\n3)Galeon\n4)Transatlantico\n";
                     cout << ">> ";
                     cin >> opc;
-                    if(opc==1)
+                    if (opc == 1)
                         tam = bote;
-                    else if(opc==2)
+                    else if (opc == 2)
                         tam = crucero;
-                    else if(opc==3)
-                        tam=galeon;
-                    else if(opc==4)
-                        tam=transatlantico;
-                    else{
-                        cout << "Ingrese opcion correcta";
+                    else if (opc == 3)
+                        tam = galeon;
+                    else if (opc == 4)
+                        tam = transatlantico;
+                    else
+                    {
+                        cout << "Ingrese una opcion correcta\n";
                         break;
                     }
                     cout << "Carga: ";
                     cin >> cantPasajeros;
-                    DtBarco* barco = new DtBarcoPasajeros(nombre,id,cantPasajeros,tam);
-                    agregarBarco(*barco);
+                    DtBarcoPasajeros barcoPas(nombre, id, cantPasajeros, tam);
+                    agregarBarco(barcoPas);
                 }
-                else{
-                    cout << "SOBAME LOS HUEVOS";
+                else
+                {
+                    cout << "Ingrese una opcion correcta"
+                         << "\n";
                 }
-        break;
-                
-    case 3: //Listar puertos
-     break;
-                
-    case 4: //Agregar arribo
-     break;
-                
-    case 5: //Obtener arribos en puerto
-     break;
-            
-    case 6: //Eliminar arribos
-     break;
-                
-    case 7: //Listar barcos
-     break;  
-                
-    case 0: //Salir
-      cout << "Saliendo...";
-     break; 
-                
-    default:
-        cout << "Opción no válida, volviendo...";
-     break;
-        }//switch
-    } //While
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+            break;
+
+        case 3: //Listar puertos
+            break;
+
+        case 4: //Agregar arribo
+            break;
+
+        case 5: //Obtener arribos en puerto
+            break;
+
+        case 6: //Eliminar arribos
+            break;
+
+        case 7: //Listar barcos
+            break;
+
+        case 0: //Salir
+            cout << "Saliendo...";
+            break;
+
+        default:
+            cout << "Opción no válida, volviendo...";
+            break;
+        } //switch
+    }     //While
 } // Main
