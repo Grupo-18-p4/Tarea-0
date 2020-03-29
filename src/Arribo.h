fd5e562899bf412ae20642c909da9ff273517700
vicente-fonfria <62406708+vicente-fonfria@users.noreply.github.com>
2020-03-29 01:53:15 -0300

    Add files via upload

---
+++ src/Arribo.h
@@ -0,0 +1,22 @@
+#ifndef _Arribo_
+#define _Arribo_
+#include <iostream>
+#include "DtFecha.h"
+#include "Barco.h"
+using namespace std;
+
+
+class Arribo {    
+ private:  
+     DtFecha fecha;    
+     float carga;
+     Barco * barco;
+ public:
+    
+    DtFecha GetArriboFecha();
+    float GetArriboCarga();
+    ~Arribo();
+    Arribo();
+    Arribo(DtFecha fecha,float carga,Barco* barco);
+ };
+ #endif
