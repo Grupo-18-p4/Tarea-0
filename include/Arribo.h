8dd225461c6123ab9d96ee40e6adece231ef2440
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-29 00:47:27 -0300

    Update Arribo.h

--- include/Arribo.h
+++ include/Arribo.h
@@ -2,15 +2,16 @@
 #define _Arribo_
 #include <iostream>
 #include "DtFecha.h"
+#include "Barco.h"
 using namespace std;
 
 
 class Arribo {    
-     private:  
+ private:  
      DtFecha fecha;    
      float carga;
- 
  public:
+     Barco * barco;  //ni idea si va en public o en private la asociacion
     DtFecha GetArriboFecha();
     float GetArriboCarga();
     ~Arribo();
