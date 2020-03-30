ed6bf67411b3196dbc1f195f539879871fe65877
nicolasyroa <62406302+nicolasyroa@users.noreply.github.com>
2020-03-29 23:37:25 -0300

    Update DtPuerto.cpp

--- src/DtPuerto.cpp
+++ src/DtPuerto.cpp
@@ -28,3 +28,8 @@ DtFecha DtPuerto :: getfecha(){
 int DtPuerto :: getcant(){
 	return this->cantArribos;
 }
+
+ostream& operator << (ostream &os,DtPuerto & dtp){
+	os << "Id puerto: " << dtp.getid() << "\n" << "Nombre: " <<  dtp.getnombre() << "\n" << "Fecha de creación: "  << dtp.getfecha().getD() << "/" << dtp.getfecha().getM() << "/" << dtp.getfecha().getA() << "\n" << "Cantidad de Arribos: " << dtp.getcant() << "\n";
+	return os;
+}
